c7de326200d7c5c04fdb20ffa6416a2eb391147e
melkerlilja <melker.c.lilja@gmail.com>
2020-02-06 10:42:36 +0100

    Initial Commit

---
+++ Labb1MelkerLiljaVG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Labb1MelkerLiljaVG
+//
+//  Created by Melker Lilja on 2020-02-06.
+//  Copyright © 2020 Melker Lilja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
